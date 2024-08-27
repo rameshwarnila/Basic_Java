7b3e11d368cf69ad268d9513fd55edfeeca4ce84
Rameshwar Nila <rameshwarnila@gmail.com>
2024-08-27 22:37:33 +0530

    20240827_Changes

---
+++ Concatenation_01.java
@@ -0,0 +1,8 @@
+package basic_java_codes;
+
+public class Concatenation_01 {
+
+	public static void main(String[] args) {
+		System.out.println("Mumbai is a humid city." + "\n" + "Weather changes are drastic.");
+	}
+}
