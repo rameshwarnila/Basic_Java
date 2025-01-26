5d2c00bc14895676a2f8902a38762df1eb1962bf
Rameshwar Nila <rameshwarnila@gmail.com>
2025-01-26 09:02:19 +0530

    LAB007_Combined

---
+++ ex_01_Java_Basic/LAB007_Combined.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+public class LAB007_Combined {
+	
+	public static void main(String[] args ) {
+		
+		System.out.println("Today is a Republic day.");
+		
+		System.out.println("Republic day is celebrated in India" + " And its on 26th Jan");
+		
+		System.out.println("It's a official holiday." + "\n" + "And the Indian flag hosting ceremony is celebrated.");
+		
+	}
+
+}
