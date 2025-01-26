e5ebd3c6977dc8a78bfb3fd32ba3700a611b52dd
Rameshwar Nila <rameshwarnila@gmail.com>
2025-01-26 15:35:53 +0530

    Comment

---
+++ ex_01_Java_Basic/LAB008_Comment.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class LAB008_Comment {
+	
+	public static void main(String[] args) {
+		
+		//This is s comment section. It will be ignored by JVM/Compiler.
+		
+		System.out.println("Hi");
+	}
+
+}
