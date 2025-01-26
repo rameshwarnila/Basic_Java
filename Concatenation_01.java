28a6e842c540697f93a0b7390320f42d363882d8
Rameshwar Nila <rameshwarnila@gmail.com>
2025-01-26 09:04:55 +0530

    Delete_Selected

--- Concatenation_01.java
+++
@@ -1,8 +0,0 @@
-package basic_java_codes;
-
-public class Concatenation_01 {
-
-	public static void main(String[] args) {
-		System.out.println("Mumbai is a humid city." + "\n" + "Weather changes are drastic.");
-	}
-}
--- First_Code.java
+++
@@ -1,8 +0,0 @@
-package basic_java_codes;
-
-public class First_Code {
-
-	public static void main (String [] args) {
-		System.out.println("Hellow World !");
-	}
-}
